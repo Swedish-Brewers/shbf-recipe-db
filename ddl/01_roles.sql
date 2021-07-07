d93b7d462ab6f6b7e3a4961d179aa6e397ac309b
Jesper St John <admin@zonezero.se>
2021-07-07 10:41:14 +0200

    Add an _import user and fix some permissions.

--- ddl/01_roles.sql
+++ ddl/01_roles.sql
@@ -29,6 +29,16 @@ BEGIN
     )
     THEN
         CREATE ROLE shbf_api WITH LOGIN;
+        GRANT shbf_reader TO shbf_api;
+    END IF;
+
+    IF NOT EXISTS
+    (
+        SELECT FROM pg_catalog.pg_roles WHERE rolname = 'shbf_import'
+    )
+    THEN
+        CREATE ROLE shbf_import WITH LOGIN;
+        GRANT shbf_writer TO shbf_import;
     END IF;
 
 END
--- ddl/05_permissions.sql
+++ ddl/05_permissions.sql
@@ -8,9 +8,11 @@
 GRANT ALL ON SCHEMA data TO shbf_writer;
 GRANT USAGE ON SCHEMA data TO shbf_reader;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA data TO shbf_writer;
+GRANT SELECT ON ALL TABLES IN SCHEMA data TO shbf_reader;
 
 -- FUNCTIONS SCHEMA
 GRANT ALL ON SCHEMA functions TO shbf_writer;
-GRANT USAGE ON SCHEMA functions TO shbf_reader, shbf_api;
+GRANT USAGE ON SCHEMA functions TO shbf_reader;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA functions TO shbf_writer;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA functions TO shbf_reader, shbf_api;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA functions TO shbf_reader;
+REVOKE ALL ON ALL FUNCTIONS IN SCHEMA functions FROM PUBLIC;
